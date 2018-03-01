b64db0e21e1d9c258e01bb1a9da21d839f364934
KeTian <ririhedou@gmail.com>
2018-03-01 18:06:33 -0500

    add model pickle

--- install.sh
+++ install.sh
@@ -16,10 +16,28 @@ sudo apt-get install tesseract-ocr
 
 echo "Your installed tesseract is located at:"
 which tesseract
-echo "Please make sure it should be in /usr/bin/tesseract\n"
+echo "Please make sure it should be in /usr/bin/tesseract!"
+echo "Did you check this?"
 
+read -p "Continue (y/n)?" choice
+case "$choice" in
+  y|Y ) echo "yes";;
+  n|N ) echo "no";;
+  * ) echo "invalid";;
+esac
+
+
+sleep 5
+
+sudo apt-get install python-tk
+
+sudo apt-get clean
+
+sleep 3
 
 echo "Install your python dependences"
+
+sudo pip install -U pytesseract==0.2.0
 
 echo "Install libraries needed for feature extraction"
 sudo pip install -U beautifulsoup4==4.6.0
@@ -38,5 +56,6 @@ sudo pip install -U numpy==1.14.1
 sudo pip install -U scipy==0.19.1
 sudo pip install -U scikit-learn==0.18.2
 
+sudo pip install -U matplotlib
 
 echo "Done!"
